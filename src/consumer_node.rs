cebae0955ea5777fbac5789f38af59f8af733050
Martín Lombardo <to@mlomb.me>
2024-11-14 13:52:23 -0300

    automatic thread count

--- src/consumer_node.rs
+++ src/consumer_node.rs
@@ -1,16 +1,27 @@
 use crate::{Networked, NodeSetup};
+use log::info;
 use std::sync::{Arc, Mutex};
 
 pub fn start_consumer_node<I, W, R>(
     setup: NodeSetup,
-    num_threads: usize,
+    mut num_threads: usize,
     func: fn(Arc<Mutex<I>>, W) -> R,
-) where
+)
+// TODO: -> Result<!, Error> when ! stabilizes
+where
     I: Networked,
     W: Networked,
     R: Networked,
 {
     let (_node, work_rx) = setup.into_consumer::<I, W, R>();
+
+    if num_threads == 0 {
+        num_threads = std::thread::available_parallelism()
+            .expect("not to fail")
+            .into();
+
+        info!("Using {} threads", num_threads);
+    }
 
     for _ in 0..num_threads {
         let work_rx = work_rx.clone();
